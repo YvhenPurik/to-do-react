38d6a4eb44dc456ca8d64acca6b7a31d1a3edada
YvhenPurik <46627779+YvhenPurik@users.noreply.github.com>
2019-01-28 07:23:50 +0200

    dell api firebase

--- src/index.js
+++ src/index.js
@@ -5,15 +5,7 @@ import App from './components/App.js';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 
-var config = {
-  apiKey: 'AIzaSyDmvJNwFuSTmH1ApALJ9AQSoS537ImGzJo',
-  authDomain: 'to-do-bf5e3.firebaseapp.com',
-  databaseURL: 'https://to-do-bf5e3.firebaseio.com',
-  projectId: 'to-do-bf5e3',
-  storageBucket: 'to-do-bf5e3.appspot.com',
-  messagingSenderId: '689970012566',
-};
-firebase.initializeApp(config);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
